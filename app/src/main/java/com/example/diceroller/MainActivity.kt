6c67c7bb58f8f55e67a9354a7c24ef6691eee1bc
Felipe Jonas <felipenjonas@gmail.com>
2021-06-20 21:08:09 -0300

    add version 2

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,9 +2,14 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +24,11 @@ class MainActivity : AppCompatActivity() {
 //            val resultTextView: TextView = findViewById(R.id.textView)
 //            resultTextView.text = "6"
             rollDice()
+
         }
 
     }
+
     /**
      * Roll the dice and update the screen with the result.
      */
@@ -29,10 +36,36 @@ class MainActivity : AppCompatActivity() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        val imageResult: ImageView = findViewById(R.id.imageView)
+        imageResult.setImageResource(R.drawable.falseicon)
+        if(diceRoll === 6) {
+            imageResult.setImageResource(R.drawable.dice6)
+        }
+        if(diceRoll === 5) {
+            imageResult.setImageResource(R.drawable.dice5)
+        }
+        if(diceRoll===4){
+            imageResult.setImageResource(R.drawable.dice4)
+        }
+        if(diceRoll===3){
+            imageResult.setImageResource(R.drawable.dice3)
+        }
+        if(diceRoll===2){
+            imageResult.setImageResource(R.drawable.dice2)
+        }
+        if(diceRoll===1){
+            imageResult.setImageResource(R.drawable.dice1)
+        }
+
+
+
         // Update the screen with the dice roll
         val resultTextView: TextView = findViewById(R.id.textView)
 //      Converting the number result to String for the use in textView
         resultTextView.text = diceRoll.toString()
+
+
+
     }
 }
 
